8f5134900fea4dc0269aa0fc3cb1a31e7cc84064
Travis Adamson <tndadamson@gmail.com>
2024-03-08 08:38:49 -0600

    Updated 5

--- Javascript_advanced/5-mode.js
+++ Javascript_advanced/5-mode.js
@@ -11,28 +11,24 @@ let darkMode = function() {changeMode(12, "bold", "capitalize", "black", "white"
 let screamMode = function() {changeMode(12, "normal", "lowercase", "white", "black")};
 
 function main() {
-	// base mode
 	let body = document.body;
 	let paragraph = document.createElement("P");
 	let text = document.createTextNode("Welcome Holberton");
 	paragraph.appendChild(text);
 	body.appendChild(paragraph);
-
-	// spooky mode 
+ 
 	let spookyButton = document.createElement("BUTTON");
 	let spookyText = document.createTextNode("Spooky");
 	spookyButton.setAttribute("onClick", "spooky()");
 	spookyButton.appendChild(spookyText);
 	body.appendChild(spookyButton);
 
-	// dark mode 
 	let darkButton = document.createElement("BUTTON");
 	let darkText = document.createTextNode("Dark mode");
 	darkButton.setAttribute("onClick", "darkMode()");
 	darkButton.appendChild(darkText);
 	body.appendChild(darkButton);
 
-	// scream mode 
 	let screamButton = document.createElement("BUTTON");
 	let screamText = document.createTextNode("Scream mode");
 	screamButton.setAttribute("onClick", "screamMode()");
